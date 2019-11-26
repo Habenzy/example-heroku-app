5db1e8d2d0e855f7eb5b97a80d8c13a4aa8862c3
Habenzy <robertrstauss@gmail.com>
2019-11-25 21:02:40 -0500

    path params!!!!!

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req, res) => {
-  res.send("Hello from the server")
+app.get('/about', (req, res) => {
+  res.sendFile('about.html')
 })
 
+app.get('/:name', (req, res) => {
+  res.send('Hello ' + req.params.name)
+})
 
 app.listen(port, function(){
   console.log('listening on port: ' + port)
